34021777b8cf11a09781986736d78f96c1dcfbfc
Meiling Bedard <meiling.bedard@gmail.com>
2022-01-26 16:05:32 -0500

    Initial Commit

---
+++ Shared/Fighting_ShipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fighting_ShipsApp.swift
+//  Shared
+//
+//  Created by Meiling Bedard on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Fighting_ShipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
