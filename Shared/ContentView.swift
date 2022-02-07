ae90e98ca9b9e34addf7d2c1736f8226b567d610
Meiling Bedard <meiling.bedard@gmail.com>
2022-02-07 12:46:39 -0500

    update home screen

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, Meiling!")
             .padding()
+        Button(action: {
+            print("New game started")
+        }) {
+            Text("Start a new game")
+        }
     }
 }
 
