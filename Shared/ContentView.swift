eba13cab34803a4df7d11e70cb015dd7b5ac303b
Meiling Bedard <meiling.bedard@gmail.com>
2022-02-14 13:37:33 -0500

    stub up home screen

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,18 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Meiling!")
-            .padding()
-        Button(action: {
-            print("New game started")
-        }) {
-            Text("Start a new game")
+        NavigationView {
+            ZStack {
+                Image("background")
+                VStack {
+                    Text("Fighting Ships")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                        .padding()
+                    Image("Yacht-rafiki")
+                        .resizable()
+                        .frame(width: 350, height: 350)
+                    Button(action: {
+                        print("Button clicked")
+                    }, label: {
+                        NavigationLink(destination: playerOneView()) {
+                    Text("Start a new game")
+                        }
+                    })
+                        .frame(width: 155, height: 50, alignment: .center)
+                        .background(Color.gray)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(15)
+                }
+            }
         }
+    }
+}
+
+struct playerOneView: View {
+    var body: some View {
+        Text("Player One")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        playerOneView()
     }
 }
+
